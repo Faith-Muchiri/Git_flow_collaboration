b55cbd2949dfc6822ba638b3f36177e1c6f24ff0
Daniel Macharia <danielhuios@gmail.com>
2022-12-21 16:10:33 +0300

    admin table migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_120619) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_120727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
